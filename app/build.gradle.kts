6f9551eeb5a2d1e45a35a52d0d7d8d2bb7d2ac89
ohult12 <ohult12@hotmail.com>
2023-09-20 11:32:56 +0200

    Calendar UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,11 @@ android {
         }
     }
     compileOptions {
+
+        //coreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+
     }
     buildFeatures {
         viewBinding = true
@@ -45,4 +48,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
--- app/src/main/java/com/example/healthappy/MainActivity.java
+++ app/src/main/java/com/example/healthappy/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.healthappy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private Button button1;
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initWidgets();
+        //selectedDate = LocalDate.now();
+        setMonthView();
         button = (Button) findViewById(R.id.loginbtn);
         button1 = (Button) findViewById(R.id.signupbtn);
         button.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +41,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    private void setMonthView() {
+        //monthYearText.setText();
+    }
+
+    /*private String monthYearFromDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+    }*/
     public void openLoginPage() {
         Intent intent = new Intent(this, loginpage.class);
         startActivity(intent);
@@ -36,5 +61,13 @@ public class MainActivity extends AppCompatActivity {
     public void openSignupPage(){
         Intent intent = new Intent(this, signuppage.class);
         startActivity(intent);
+    }
+
+    public void previousMonthAction(View view) {
+
+    }
+
+    public void nextMonthAction(View view) {
+
     }
 }
