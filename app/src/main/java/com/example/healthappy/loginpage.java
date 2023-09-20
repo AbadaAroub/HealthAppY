625d5999bd82b5bb70630cf38a7ae9c62af0ab40
Oskar Deubler <deubler.oskar@gmail.com>
2023-09-20 12:39:43 +0200

    Firebase Authetication Integrated, also i did a merge :) hope it works lul

--- app/src/main/java/com/example/healthappy/loginpage.java
+++ app/src/main/java/com/example/healthappy/loginpage.java
@@ -1,14 +1,40 @@
 package com.example.healthappy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class loginpage extends AppCompatActivity {
+    EditText editTextEmail, editTextPassword;
+    Button loginbutton;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), signuppage.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +48,50 @@ public class loginpage extends AppCompatActivity {
             }
         });
 
+
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.username); //Change to email?
+        editTextPassword = findViewById(R.id.password);
+        loginbutton = findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(loginpage.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(loginpage.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(loginpage.this, "Login Successful",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(loginpage.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
     }
     public void openmealmanagmentpage() {
         Intent intent = new Intent(this, mealmanagment.class);
