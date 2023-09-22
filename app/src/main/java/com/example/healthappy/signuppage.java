2ca24264461683f78c523378d371b31c9aecf1fa
Filip Karlsson <filikarl104@student.kau.se>
2023-09-22 11:53:14 +0200

    ver1

--- app/src/main/java/com/example/healthappy/signuppage.java
+++ app/src/main/java/com/example/healthappy/signuppage.java
@@ -17,11 +17,24 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class signuppage extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
 
+public class signuppage extends AppCompatActivity {
     EditText editTextEmail, editTextPassword, editTextRePassword;
     Button signUpBtn;
     FirebaseAuth mAuth;
+
+    //Database
+    private EditText caregiverNameEdt, caregiverMobileEdt;
+    Caregiver caregiver;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     public void onStart() {
@@ -36,16 +49,29 @@ public class signuppage extends AppCompatActivity {
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.signuppass);
         editTextRePassword = findViewById(R.id.resignuppass);
+
+        //Database
+        caregiverNameEdt = findViewById(R.id.username1);
+        caregiverMobileEdt = findViewById(R.id.mobileNumber);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("Caregiver");
+        caregiver = new Caregiver();
+
         signUpBtn = findViewById(R.id.signup);
 
         signUpBtn.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View view) {
                 String email, password, rePassword;
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
                 rePassword = String.valueOf(editTextRePassword.getText());
+
+                //Database
+                String name = caregiverNameEdt.getText().toString();
+                String number = caregiverMobileEdt.getText().toString();
+                String uid = user.getUid();
 
                 if (TextUtils.isEmpty(email)){
                     Toast.makeText(signuppage.this, "Enter Email", Toast.LENGTH_SHORT).show();
@@ -59,6 +85,13 @@ public class signuppage extends AppCompatActivity {
                     Toast.makeText(signuppage.this, "Reenter Password", Toast.LENGTH_SHORT).show();
                 } else if (!String.valueOf(editTextPassword.getText()).equals(String.valueOf(editTextRePassword.getText()))){
                     Toast.makeText(signuppage.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+                }
+
+                //Database
+                if(TextUtils.isEmpty(name) && TextUtils.isEmpty(number)) {
+                    Toast.makeText(signuppage.this, "Add some data:", Toast.LENGTH_SHORT).show();
+                } else {
+                    addDatatoFirebase(name, number, uid);
                 }
 
                 mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -78,6 +111,25 @@ public class signuppage extends AppCompatActivity {
                                 }
                             }
                 });
+            }
+        });
+    }
+
+    //Database
+    private void addDatatoFirebase(String name, String number, String uid) {
+        caregiver.setName(name);
+        caregiver.setMobile_nr(number);
+        caregiver.setUser_UID(uid);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                databaseReference.setValue(caregiver);
+                Toast.makeText(signuppage.this, "Data added", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(signuppage.this, "Fail to add data " + error, Toast.LENGTH_SHORT).show();
             }
         });
     }
