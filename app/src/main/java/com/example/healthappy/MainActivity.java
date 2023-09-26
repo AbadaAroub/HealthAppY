d68620228ddc050ca7d59435a1b9ec3de11f259d
Oskar Deubler <deubler.oskar@gmail.com>
2023-09-26 10:31:38 +0200

    refactoring code, build works now

--- app/src/main/java/com/example/healthappy/MainActivity.java
+++ app/src/main/java/com/example/healthappy/MainActivity.java
@@ -7,19 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import android.widget.TextView;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private Button button1;
+    private Button loginBtn;
+    private Button signupBtn;
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
     private LocalDate selectedDate;
@@ -31,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
         initWidgets();
         selectedDate = LocalDate.now();
         setMonthView();
-        button = (Button) findViewById(R.id.loginbtn);
-        button1 = (Button) findViewById(R.id.signupbtn);
-        button.setOnClickListener(new View.OnClickListener() {
+        loginBtn = (Button) findViewById(R.id.loginbtn);
+        signupBtn = (Button) findViewById(R.id.signupbtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
